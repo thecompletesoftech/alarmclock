abd0a17d43763a5c8388622075dee5eee10f01ab
abhi singh <abhi1124365@gmail.com>
2024-01-17 03:43:26 -0800

    Add fonts

--- lib/Account/Login.dart
+++ lib/Account/Login.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+import '../Config/TextStyle.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -8,12 +11,46 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Center(child: Image.asset("assets/TivalLogo.png")),
+              SizedBox(
+                height: 17,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Login",
+                    style: LoginTextStyle,
+                  ),
+                ],
+              ),
+              Neumorphic(
+                margin: EdgeInsets.only(left: 8, right: 8, top: 2, bottom: 4),
+                style: NeumorphicStyle(
+                  depth: NeumorphicTheme.embossDepth(context),
+                  boxShape: NeumorphicBoxShape.stadium(),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 14, horizontal: 18),
+                child: TextField(
+                    onChanged: (value) {
+                      
+                    },
+                    controller: _controller,
+                    // decoration: InputDecoration.collapsed(hintText: this.widget.hint),
+                    ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/Config/TextStyle.dart
@@ -0,0 +1,9 @@
+import 'package:clockalarm/Config/Import.dart';
+
+const LoginTextStyle = TextStyle(
+  fontFamily: "Lato",
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+  color: Color(0xff53967a),
+  height: 26 / 24,
+);
