168264cc5eac065abdec950896720413c01ac0ee
Abhishek Singh <abhi1124365@gmail.com>
2024-01-18 15:43:15 -0800

    imort issue

--- lib/Config/Import.dart
+++ lib/Config/Import.dart
@@ -1,7 +1,5 @@
-export 'package:flutter/material.dart';
 export 'package:clockalarm/Widgets/TextBoxwidget.dart';
 export 'package:clockalarm/Config/Color.dart';
-export 'package:clockalarm/Account/Login.dart';
 export 'package:clockalarm/Config/Helper.dart';
 export 'package:clockalarm/View/Auth/SignIn/SignIn.dart';
 export 'package:flutter_neumorphic_plus/flutter_neumorphic.dart';
--- lib/View/SplashScreen.dart
+++ lib/View/SplashScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import '../Config/Import.dart';
 
 class SplashScreen extends StatefulWidget {
