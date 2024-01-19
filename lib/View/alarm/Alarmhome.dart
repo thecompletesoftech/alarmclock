03680e8d67fc86394ea09c33c276ec40c786ac35
Abhishek Singh <abhi1124365@gmail.com>
2024-01-18 19:13:53 -0800

    alarm design

--- lib/View/alarm/Alarmhome.dart
+++ lib/View/alarm/Alarmhome.dart
@@ -1,5 +1,4 @@
 import '../../Config/Import.dart';
-import '../../Config/Utils.dart';
 
 class AlarmHome extends StatefulWidget {
   const AlarmHome({super.key});
@@ -12,8 +11,6 @@ class _AlarmHomeState extends State<AlarmHome> {
   bool isSwitched = false;
   @override
   Widget build(BuildContext context) {
-    double baseWidth = 390;
-    double fem = MediaQuery.of(context).size.width / baseWidth;
     return Scaffold(
       appBar: PreferredSize(
           preferredSize: Size.fromHeight(100),
@@ -29,41 +26,7 @@ class _AlarmHomeState extends State<AlarmHome> {
             SizedBox(
               height: 10,
             ),
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                Container(
-                  height: 230,
-                  width: 230,
-                  decoration: Utils().decoration(
-                      cntx: context,
-                      isdark: NeumorphicTheme.isUsingDark(context),
-                      radius: 250.0 * fem),
-                ),
-                Container(
-                  height: 150,
-                  width: 150,
-                  child: AnalogClock(
-                    decoration: Utils().decoration(
-                        cntx: context,
-                        isdark: NeumorphicTheme.isUsingDark(context),
-                        radius: 150 * fem),
-                    width: 150.0,
-                    isLive: true,
-                    hourHandColor: Colors.white,
-                    minuteHandColor: NeumorphicTheme.accentColor(context),
-                    showSecondHand: false,
-                    numberColor: Colors.black87,
-                    showNumbers: false,
-                    showAllNumbers: false,
-                    textScaleFactor: 1.4,
-                    showTicks: false,
-                    showDigitalClock: false,
-                    datetime: DateTime(2019, 1, 1, 9, 12, 15),
-                  ),
-                ),
-              ],
-            ),
+            Clock(),
             SizedBox(height: 50),
             ListView.builder(
               physics: NeverScrollableScrollPhysics(),
@@ -73,7 +36,7 @@ class _AlarmHomeState extends State<AlarmHome> {
                 return AlramCard(
                   medium: 'pm',
                   time: '6:45',
-                ).paddingOnly(bottom: 20, top: 10);
+                ).paddingOnly(bottom: 10, top: 10);
               },
             )
           ],
---
+++ lib/View/alarm/Clock.dart
@@ -0,0 +1,138 @@
+import 'package:clockalarm/View/WorldClock/WorldClock.dart';
+
+import '../../Config/Import.dart';
+
+class Clock extends StatefulWidget {
+  const Clock({super.key});
+
+  @override
+  State<Clock> createState() => _ClockState();
+}
+
+class _ClockState extends State<Clock> {
+  @override
+  Widget build(BuildContext context) {
+    double baseWidth = 390;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Stack(
+          children: [
+            Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Container(
+                  height: 230,
+                  width: 230,
+                  decoration: Utils().decoration(
+                      cntx: context,
+                      isdark: NeumorphicTheme.isUsingDark(context),
+                      radius: 250.0 * fem),
+                ),
+                Container(
+                  height: 18,
+                  width: 2,
+                  color: Colors.white,
+                ).paddingOnly(top: 8),
+              ],
+            ),
+            Stack(
+              alignment: Alignment.centerLeft,
+              children: [
+                Container(
+                  height: 230,
+                  width: 230,
+                  color: Colors.transparent,
+                ),
+                Container(
+                  height: 2,
+                  width: 18,
+                  color: Colors.white,
+                ).paddingOnly(left: 8),
+              ],
+            ),
+            Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Container(
+                  height: 230,
+                  width: 230,
+                  color: Colors.transparent,
+                ),
+                Container(
+                  height: 18,
+                  width: 2,
+                  color: Colors.white,
+                ).paddingOnly(bottom: 8),
+              ],
+            ),
+            Stack(
+              alignment: Alignment.centerRight,
+              children: [
+                Container(
+                  height: 230,
+                  width: 230,
+                  color: Colors.transparent,
+                ),
+                Container(
+                  height: 2,
+                  width: 18,
+                  color: Colors.white,
+                ).paddingOnly(right: 8),
+              ],
+            ),
+          ],
+        ),
+        Container(
+          height: 150,
+          width: 150,
+          child: AnalogClock(
+            decoration: Utils().decoration(
+                cntx: context,
+                isdark: NeumorphicTheme.isUsingDark(context),
+                radius: 150 * fem),
+            width: 150.0,
+            isLive: true,
+            hourHandColor: Colors.white,
+            minuteHandColor: NeumorphicTheme.accentColor(context),
+            showSecondHand: false,
+            numberColor: Colors.black87,
+            showNumbers: false,
+            showAllNumbers: false,
+            textScaleFactor: 1.4,
+            showTicks: false,
+            showDigitalClock: false,
+            datetime: DateTime(2019, 1, 1, 9, 12, 15),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget stackwidget({padding, hight, width, Alignment}) {
+    double baseWidth = 390;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+    return Stack(
+      alignment: Alignment,
+      children: [
+        Container(
+          height: 230,
+          width: 230,
+          decoration: Utils().decoration(
+              cntx: context,
+              isdark: NeumorphicTheme.isUsingDark(context),
+              radius: 250.0 * fem),
+        ),
+        Padding(
+          padding: padding,
+          child: Container(
+            height: hight,
+            width: width,
+            color: Colors.white,
+          ),
+        )
+      ],
+    );
+  }
+}
