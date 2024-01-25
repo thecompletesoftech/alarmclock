ed87e7d1fac32b5e93a8c6ef62ff0494780d2a8c
RishabhwithGod <rishabh72149@gmail.com>
2024-01-25 14:54:07 +0530

    add world clock Done

--- lib/Config/Helper.dart
+++ lib/Config/Helper.dart
@@ -1,4 +1,5 @@
 import 'package:clockalarm/Config/Import.dart';
+import 'package:intl/intl.dart';
 
 nextscreenwithoutback(cntx, screenname) {
   Navigator.pushReplacement(
@@ -16,4 +17,33 @@ backscreen(cntx) {
 
 buttonradius({r}) {
   NeumorphicBoxShape.roundRect(BorderRadius.circular(r ?? 15.0));
+}
+
+String formatDate(String? dateString) {
+  if (dateString != null) {
+    DateTime dateTime = DateTime.parse(dateString);
+    String formattedDate = DateFormat('dd MMMM yyyy').format(dateTime);
+    return formattedDate;
+  } else {
+    return "date";
+  }
+}
+
+formatTime(dateString) {
+  if (dateString != null) {
+    String formattedDate = DateFormat('hh:mm a').format(dateString);
+    return formattedDate;
+  } else {
+    return "date";
+  }
+}
+
+String formatDateTime(String? dateString) {
+  if (dateString != null) {
+    DateTime dateTime = DateTime.parse(dateString);
+    String formattedDate = DateFormat('yyyy-MM-dd hh:mm a').format(dateTime);
+    return formattedDate;
+  } else {
+    return "date";
+  }
 }
